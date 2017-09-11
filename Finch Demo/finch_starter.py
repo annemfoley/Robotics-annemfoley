1ab42c9d06baa9415825aca7ecf6a733946e1b0b
BuildTools <unconfigured@null.spigotmc.org>
2017-09-10 21:08:09 -0500

    Finch Intro
    have basic framework, use linear programming and plan out activity

---
+++ Finch Demo/finch_starter.py
@@ -0,0 +1,27 @@
+# An introduction to programming: Finch Robots
+# Huskie Robotics 9/27/17
+
+
+import Finch
+import time
+
+class Finch():
+    def __init__(self):
+        self.connection = finchconnection.ThreadedFinchConnection()
+        self.connection.open()
+
+    
+finch = Finch()
+
+
+
+
+
+
+
+
+
+
+
+finch.halt()
+finch.close()
