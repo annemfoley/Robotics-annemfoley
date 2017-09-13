7ae40001b52d6bc3078fa8c2e4e9f53a551b6fed
BuildTools <unconfigured@null.spigotmc.org>
2017-09-12 22:50:55 -0500

    Finished Slides
    make sure to demo along with the slides tomorrow

--- Finch Demo/finch_starter.py
+++ Finch Demo/finch_starter.py
@@ -1,17 +1,11 @@
-# An introduction to programming: Finch Robots
-# Huskie Robotics 9/27/17
-
 
 import finch
 import time
 
 
-
-
-    
 def main():
     bot = finch.Finch()
-    #put code here, make sure to indent
+
 
 
     
